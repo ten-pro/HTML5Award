07324ae5d87c3af3613dcf2c42f458ab4a0d94cd
明石凜太 <jeffrey.bezos@icloud.com>
2023-01-16 11:40:57 +0900

    Ranking 完成

--- engineergame/src/components/Ranking.vue
+++ engineergame/src/components/Ranking.vue
@@ -1,25 +1,58 @@
 <template>
     <div>
-        <div class="rankingRow">
-            <div class="ranking1">
-                <div class="rankingName1" v-vind="score[0].name">{{ score[0].name != null ? score[0].name : "-" }}</div>
+        <div class="text">RESULT</div>
+        <div class="rankingList">
+            <div class="rankingNumberBack1 gold">
+                <div class="rankingNumber1 whiteFore">1</div>
+            </div>
+            <div class="rankingMainBack1">
+                <div class="rankingName1">{{ score[0].name != null ? score[0].name : "noName" }}</div>
                 <div class="rankingScore1">{{ score[0].employee != null ? score[0].employee : "-" }}</div>
             </div>
-            <div class="ranking2">
-                <div class="rankingName2">{{ score[1].name != null ? score[1].name : "-" }}</div>
-                <div class="rankingScore2">{{ score[1].employee != null ? score[1].employee : "-" }}</div>
+        </div>
+        <div class="rankingList">
+            <div class="rankingNumberBack1 silver">
+                <div class="rankingNumber1 whiteFore">2</div>
             </div>
-            <div class="ranking3">
-                <div class="rankingName3">{{ score[2].name != null ? score[2].name : "-" }}</div>
-                <div class="rankingScore3">{{ score[2].employee != null ? score[2].employee : "-" }}</div>
+            <div class="rankingMainBack1">
+                <div class="rankingName1">{{ score[1].name != null ? score[1].name : "noName" }}</div>
+                <div class="rankingScore1">{{ score[1].employee != null ? score[1].employee : "-" }}</div>
             </div>
-            <div class="ranking4">
-                <div class="rankingName4">{{ score[3].name != null ? score[3].name : "-" }}</div>
-                <div class="rankingScore4">{{ score[3].employee != null ? score[3].employee : "-" }}</div>
+        </div>
+        <div class="rankingList">
+            <div class="rankingNumberBack1 bronze">
+                <div class="rankingNumber1 whiteFore">3</div>
             </div>
-            <div class="ranking5">
-                <div class="rankingName5">{{ score[4].name != null ? score[4].name : "-" }}</div>
-                <div class="rankingScore5">{{ score[4].employee != null ? score[4].employee : "-" }}</div>
+            <div class="rankingMainBack1">
+                <div class="rankingName1">{{ score[2].name != null ? score[2].name : "noName" }}</div>
+                <div class="rankingScore1">{{ score[2].employee != null ? score[2].employee : "-" }}</div>
+            </div>
+        </div>
+        <div class="rankingList">
+            <div class="rankingNumberBack1 whiteBack">
+                <div class="rankingNumber1 black">4</div>
+            </div>
+            <div class="rankingMainBack1">
+                <div class="rankingName1">{{ score[3].name != null ? score[3].name : "noName" }}</div>
+                <div class="rankingScore1">{{ score[3].employee != null ? score[3].employee : "-" }}</div>
+            </div>
+        </div>
+        <div class="rankingList">
+            <div class="rankingNumberBack1 whiteBack">
+                <div class="rankingNumber1 black">5</div>
+            </div>
+            <div class="rankingMainBack1">
+                <div class="rankingName1">{{ score[4].name != null ? score[4].name : "noName" }}</div>
+                <div class="rankingScore1">{{ score[4].employee != null ? score[4].employee : "-" }}</div>
+            </div>
+        </div>
+        <div class="rankingList">
+            <div class="rankingNumberBack1 whiteBack">
+                <div class="rankingNumber1 black">⭐︎</div>
+            </div>
+            <div class="rankingMainBack1">
+                <div class="rankingName1">MyScore</div>
+                <div class="rankingScore1">{{ score[4].employee != null ? score[4].employee : "-" }}</div>
             </div>
         </div>
     </div>
@@ -60,87 +93,80 @@ import axios from "axios"
 
 </script>
 <style scoped>
-.rankingRow {
-    margin-top: 12%;
+.text {
+    text-align: center;
+    color: #943BED;
+    font-size: 90px;
+    font-weight: bold;
+    letter-spacing: 10px;
 }
-.ranking1 {
+.rankingList {
     display: flex;
-    margin-left: 25%;
+width: 100%;
+margin: 30px 0 0 25%;
+}
+.rankingNumberBack1 {
+margin: 50px -20px 0 20px ;
+transform: skewX(30deg);
+    width: 80px;
+    height: 50px;
+    text-align: center;
+    position: relative;
+    
+}
+.rankingNumber1 {
+position: absolute;
+transform: skewX(-30deg);
+width: 100%;
+height: 100%;
+top: -10px;
+left: 8px;
+text-align: center;
+font-size: 45px;
+font-weight:bolder;
+font-family: serif;
+}
+.rankingMainBack1 {
+    position: relative;
+    transform: skewX(30deg);
+    width: 800px;
+    height: 100px;
+background: linear-gradient(to bottom right,#943BED,#FF97D5);
 }
 .rankingName1 {
-    font-size: 60px;
-    margin-right: 40%;
-    font-weight: bold;
-    color: white;
+position: absolute;
+transform: skewX(-30deg);
+margin: 0 auto;
+font-size: 60px;
+color: white;
+left: 15%;
+font-weight: bold;
 }
 .rankingScore1 {
+    position: absolute;
+    transform: skewX(-30deg);
     font-size: 60px;
-    font-weight: bold;
-    color: white;
+    color: black;
+left: 66%;
+font-weight: bold;
 }
-.ranking2 {
-    display: flex;
-    margin-top: 3%;
-    margin-left: 25%;
+
+.gold {
+    background-color: #dab300;
 }
-.rankingName2 {
-    font-size: 60px;
-    margin-right: 40%;
-    font-weight: bold;
-    color: white;
+.silver {
+    background-color: #BDC3C9;
 }
-.rankingScore2 {
-    font-size: 60px;
-    font-weight: bold;
-    color: white;
+.bronze {
+    background-color: #9A6229;
 }
-.ranking3 {
-    display: flex;
-    margin-top: 3%;
-    margin-left: 25%;
+.whiteBack {
+    background-color: #FFFFFF;
 }
-.rankingName3 {
-    font-size: 60px;
-    margin-right: 40%;
-    font-weight: bold;
-    color: white;
+.black {
+    color: black;
 }
-.rankingScore3 {
-    font-size: 60px;
-    font-weight: bold;
-    color: white;
-}
-.ranking4 {
-    display: flex;
-    margin-top: 4%;
-    margin-left: 25%;
-}
-.rankingName4 {
-    
-    font-size: 60px;
-    margin-right: 40%;
-    font-weight: bold;
-    color: white;
-}
-.rankingScore4 {
-    font-size: 60px;
-    font-weight: bold;
-    color: white;
-}
-.ranking5 {
-    display: flex;
-    margin-top: 4%;
-    margin-left: 25%;
-}
-.rankingName5 {
-    font-size: 60px;
-    margin-right: 40%;
-    font-weight: bold;
-    color: white;
-}
-.rankingScore5 {
-    font-size: 60px;
-    font-weight: bold;
+.whiteFore {
     color: white;
 }
 </style>
