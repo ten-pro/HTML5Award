e9feed8876785f6c6a284d1b2efce9f8ec4528ab
kanapiii <113953054+kanapiii@users.noreply.github.com>
2023-01-18 09:34:59 +0900

    login成功

--- engineergame/src/components/login/Login.vue
+++ engineergame/src/components/login/Login.vue
@@ -8,20 +8,31 @@
         <input type="pass" class="login_pass" v-model="pass">
 
         <p class="error1">ユーザー名が登録されていないか、パスワードが間違っています</p>
-        <button class="logbtn" @click="login">login</button>
+        <button class="logbtn" @click="loginchk">login</button>
         <a href=""><button class="createbtn">create new</button></a>
     </div>
 </template>
 <script setup>
+import axios from 'axios'
 import { reactive } from "vue"
 let name=reactive("");
 let pass=reactive("");
 let display=reactive(["none"]);
-const login =()=>{
-        return {
-            user_name:name,
-            user_pass:pass
-        }
+let loginchk =()=>{
+            axios
+                .post('https://mp-class.chips.jp/engineergame/Loginmain.php', {
+                    user_name: name,
+                    user_pass: pass,
+                    login_user: ''
+                }, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                })
+                .then(
+                    (response) => (loginchk = response.data),
+                    console.log("成功")
+                )
 };
 </script>
 <style>
