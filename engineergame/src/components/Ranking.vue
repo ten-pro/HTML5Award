ab1d534e78e608bbaa748439d8b86c9173ec3a71
明石凜太 <jeffrey.bezos@icloud.com>
2023-01-19 01:22:36 +0900

    home button

--- engineergame/src/components/Ranking.vue
+++ engineergame/src/components/Ranking.vue
@@ -55,6 +55,7 @@
                 <div class="rankingScore1">{{ score[4].employee != null ? score[4].employee : "-" }}</div>
             </div>
         </div>
+        <button class="custom-btn btn-9 startbutton" @click="home">←ホームへ</button>
     </div>
 </template>
 <script setup>
@@ -101,14 +102,58 @@ import axios from "axios"
                 // myScore = response.data
             }
         )
-    }
 
+    }
+const home = () => {
+    location.href = "/home"
+}
 
 
 
 
 </script>
 <style scoped>
+.custom-btn {
+    width: 300px;
+    height: 100px;
+    color: #fff;
+    border-radius: 5px;
+    margin:2% 0 0 5%;
+    padding: 10px 25px;
+    font-family: 'Lato', sans-serif;
+    font-weight: 500;
+    font-size: 50px;
+    background: transparent;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    position: relative;
+    display: inline-block;
+    box-shadow:inset 2px 2px 2px 0px rgba(255,255,255,.5),
+    7px 7px 20px 0px rgba(0,0,0,.1),
+    4px 4px 5px 0px rgba(0,0,0,.1);
+    outline: none;
+  }
+  
+  .btn-9 {
+    border: none;
+    transition: all 0.3s ease;
+    overflow: hidden;
+  }
+  .btn-9:after {
+    position: absolute;
+    content: " ";
+    z-index: -1;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+     background-color: #1fd1f9;
+  background-image: linear-gradient(315deg, #1fd1f9 0%, #b621fe 85%);
+    transition: all 0.3s ease;
+  }
+.btn-9:hover {
+    background-image: linear-gradient(315deg, #b621fe 0%, #1fd1f9 85%);
+}
 .text {
     text-align: center;
     color: #943BED;
